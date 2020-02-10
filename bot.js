22378b20d676ffd9243c7396a859a90ef5272dd3
omar0700bot <59139063+omar0700bot@users.noreply.github.com>
2020-02-10 13:10:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,14 +17,14 @@ client.on("message", message => {
 
 
 client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`veit ..`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
+console.log(`Logged in as ${client.user.tag}!`);
+console.log("Streamstatus by DayZoonHD")
+
+client.user.setActivity(`Avatar.`, {
+type: "Playing",
+url: "https://www.twitch.tv/ee"})
+    .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'none'}`))
+    .catch(console.error);
 });
 
 
