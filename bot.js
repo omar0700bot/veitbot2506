9056799bad1eeed271e42b2a48648d1ecd282516
omar0700bot <59139063+omar0700bot@users.noreply.github.com>
2020-02-10 13:18:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,7 @@ client.on("message", message => {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach
 })
  message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
  message.delete(); 
